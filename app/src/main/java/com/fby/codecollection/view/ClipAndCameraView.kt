c347ee9bfef7c4892f26bea9718347695547a3a4
bojan <fengboyang@sina.com>
2021-09-14 17:25:57 +0800

    clip camera

---
+++ app/src/main/java/com/fby/codecollection/view/ClipAndCameraView.kt
@@ -0,0 +1,64 @@
+package com.fby.codecollection.view
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.fby.codecollection.R
+import com.fby.codecollection.util.dp
+
+/**
+
+ * 作者：YS010051USR on 2021/9/13 14:10
+
+ * 邮箱：fengboyang0412@gmail.com
+
+ */
+class ClipAndCameraView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val BITMAP_SIZE = 200.dp
+    private val BITMAP_PADDING = 100.dp
+    private var mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val bitmap = getAvatar(BITMAP_SIZE.toInt())
+    private val camera = Camera()
+    private val clipPath = Path().apply {
+        addOval(0f, 0f, BITMAP_SIZE, BITMAP_SIZE, Path.Direction.CCW)
+    }
+
+    init {
+        camera.rotateX(30f)
+        camera.setLocation(0f, 0f, -6 * resources.displayMetrics.density)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //先移动后旋转,写的时候下面先旋转
+        canvas.save()
+        canvas.clipRect(
+            BITMAP_PADDING,
+            BITMAP_PADDING,
+            BITMAP_PADDING + BITMAP_SIZE,
+            BITMAP_PADDING + BITMAP_SIZE / 2
+        )
+        canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING, mPaint)
+        canvas.restore()
+
+        canvas.save()
+        canvas.translate(BITMAP_PADDING + BITMAP_SIZE / 2, BITMAP_PADDING + BITMAP_SIZE / 2)
+        camera.applyToCanvas(canvas)
+        canvas.clipRect(-BITMAP_SIZE / 2, 0f, BITMAP_SIZE / 2, BITMAP_SIZE / 2)
+        canvas.translate(-(BITMAP_PADDING + BITMAP_SIZE / 2), -(BITMAP_PADDING + BITMAP_SIZE / 2))
+        canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING, mPaint)
+        canvas.restore()
+    }
+
+    private fun getAvatar(width: Int): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+        options.inJustDecodeBounds = false
+        options.inDensity = options.outWidth
+        options.inTargetDensity = width
+        return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+    }
+}
